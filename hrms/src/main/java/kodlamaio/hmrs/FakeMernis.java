fdb31209334e1e3d09b0099d752c5303ab6075c8
emrea <emrea@192.168.1.22>
2021-05-28 23:09:07 +0300

    İsterler yerine getirildi

--- hrms/src/main/java/kodlamaio/hmrs/FakeMernis.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hmrs;
-
-public class FakeMernis {
-	public boolean checkUser(String nationaltyId, String firstName, String lastName, String dateOfBirth) {
-		FakeMernis fakeMernis = new FakeMernis();
-		return fakeMernis.checkUser(nationaltyId, firstName, lastName, dateOfBirth);
-	}
-	
-}
--- hrms/src/main/java/kodlamaio/hmrs/core/adapters/abstracts/FakeMernisService.java
+++
@@ -1,8 +0,0 @@
-package kodlamaio.hmrs.core.adapters.abstracts;
-
-import kodlamaio.hmrs.entities.concretes.User;
-
-public interface FakeMernisService {
-	boolean checkUser(String nationaltyId, String firstName,String lastName,String dateOfBirth);
-
-}
